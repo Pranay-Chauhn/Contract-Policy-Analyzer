6ed6bedb3da4b7fd62ae674767d11128029ec7b5
Pranay-Chauhn <pranaychauhan663@gmail.com>
2025-06-25 13:59:32 +0530

    added env file

--- chains/summarizer.py
+++ chains/summarizer.py
@@ -4,11 +4,16 @@ from langchain_huggingface.embeddings import HuggingFaceEmbeddings
 from langchain.chains import LLMChain
 from openai import base_url
 from utils.prompt import get_contract_prompt, get_policy_prompt
+from dotenv import load_dotenv
+import os
+
+# load env
+load_dotenv()
 
 llm = ChatOpenAI(
-    base_url="http://192.168.1.34:1234/v1",
-    api_key="lmstudio",
-    model_name="meta-llama-3.1-8b-instruct"
+    base_url=os.getenv("LLM_API_BASE_URL"),
+    api_key=os.getenv("LLM_API_KEY"),
+    model_name=os.getenv("LLM_MODEL_NAME")
 )
 
 
