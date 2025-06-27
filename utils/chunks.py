2440fafe40b9f1a14eddce444cd8c844334948fd
Pranay-Chauhn <pranaychauhan663@gmail.com>
2025-06-27 16:48:48 +0530

    Keyword error

--- utils/chunks.py
+++ utils/chunks.py
@@ -6,4 +6,4 @@ def split_to_chunks(text, chunk_size=1000, chunk_overlap=100):
         chunk_size=chunk_size,
         chunk_overlap=chunk_overlap
     )
-    return spliter.create_document([text])
+    return spliter.split_documents([text])
